b98c6596b4ea9da06ab045ad0e6ea71a2e700206
George Raymond <geomarray@gmail.com>
2018-05-15 16:38:35 -0700

    console log the formatted results of the database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,38 @@
-var mysql = require("msql");
+var mysql = require("mysql");
+var inquirer = require('inquirer');
+
 
 var connection = mysql.createConnection({
-    host:"",
-    port:3307,
-})
+    host: "localhost",
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "Smokethedog1992!!",
+    database: "bamazon"
+  });
+
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    readProducts()
+  });
+
+  function readProducts() {
+    console.log("here is a list of product that we have for sale...\n");
+    connection.query("SELECT * FROM products", function(err, res) {
+      if (err) throw err;
+      // Log all results of the SELECT statement
+      for (i = 0; i<res.length;i++){
+        console.log("\nItem ID#: " + res[i].id);
+        console.log("Product: " + res[i].product_name);
+        console.log("Product Department: " + res[i].department_name);
+        console.log("Price: $" + res[i].price);
+        console.log("Units In Stock: " + res[i].stock_quantity);
+      }
+      
+      connection.end();
+    });
+  }
