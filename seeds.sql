5e8e9e94551a3852c319d2bac82eb1e317de6f1a
George Raymond <geomarray@gmail.com>
2018-05-15 16:11:04 -0700

    updated the seeds.sql

--- seeds.sql
+++ seeds.sql
@@ -2,13 +2,14 @@ CREATE DATABASE bamazon;
 
 USE bamazon;
 
-CREATE TABLE products (
-id INT(15) AUTO_INCREMENT NOT NULL,
+CREATE TABLE products(
+id INT NOT NULL AUTO_INCREMENT,
 product_name VARCHAR(30) NOT NULL,
 department_name VARCHAR(30) NOT NULL,
 price INT(15) NULL,
-stock_quantity INT(10) NOT NULL
-)
+stock_quantity INT(10) NOT NULL,
+PRIMARY KEY (id)
+);
 
 INSERT INTO products(product_name, department_name, price, stock_quantity)
 VALUES ("bannana" , "food" , 2 , 50);
